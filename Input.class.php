e1eed3f6c9cf3286bfac80228174521a860c7511
spake <spaake123@gmail.com>
2015-10-10 22:44:06 +0200

    A class to read SPOJ STDIN

---
+++ Input.class.php
@@ -0,0 +1,27 @@
+<?php
+class Input{
+	
+private $stdin;
+
+	public function __construct(){
+	$this->stdin = fopen('php://stdin', 'r');
+	}
+	
+	public function read(){
+		//fscanf($this->stdin, "%d", $n);
+		$n = fgets($this->stdin);
+		return $n;
+	}
+	
+	public function read_line($ile_liczb){
+		$tab;
+			$line = fgets($this->stdin);
+			$tab[] = explode(" ", $line);
+
+		return $tab;
+	}
+	
+}
+
+
+?>
